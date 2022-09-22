75f0efce15f44b23651cc625bb14833ecbb70e84
bukvic6 <bukvic6@gmail.com>
2022-09-22 21:15:59 +0200

    register design

--- src/pages/home/Login.jsx
+++ src/pages/home/Login.jsx
@@ -3,17 +3,17 @@ import "./login.css"
 
 export default function Login() {
   return (
-    <div className='login'>
+    <div className="login">
         <div className="loginWrapper">
             <div className="pictureLeft">
             <img src="/assets/login2.jpg" alt="" className="loginpic" />
             </div>
             <div className="form">
             <label for="uname"><b>Username</b></label>
-            <input className='logininput' type="text" placeholder="Enter Username" name="uname"/>
+            <input className="logininput" type="text" placeholder="Enter Username" name="uname"/>
 
             <label for="psw"><b>Password</b></label>
-            <input className='logininput' type="password" placeholder="Enter Password" name="psw" />
+            <input className="logininput" type="password" placeholder="Enter Password" name="psw" />
 
             <button className='buttonLogin' type="submit">Login</button>
             <label>
--- src/pages/home/Register.jsx
+++ src/pages/home/Register.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import "./register.css"
 
 export default function Register() {
   return (
-    <div>Register</div>
+        <div className="register">
+        <div className="registerWrapper">
+            <div className="regPicLeft">
+            <img src="/assets/register2.png" alt="" className="registerpic" />
+            </div>
+            <div className="form">
+            <label for="uname"><b>Username</b></label>
+            <input className="logininput" type="text" placeholder="Enter Username" name="uname"/>
+
+            <label for="email"><b>Email</b></label>
+            <input className="logininput" type="password" placeholder="Enter Email" name="email" />
+
+            <label for="psw"><b>Password</b></label>
+            <input className="logininput" type="password" placeholder="Enter Password" name="psw" />
+
+            <button className="buttonRegister" type="submit">Register</button>
+            <label>
+            </label>
+            </div>
+        </div>
+    </div>
   )
 }
