3aa6b6dd5dddb1b2e3302c5770dadc7e579fe42b
bukvic6 <bukvic6@gmail.com>
2022-09-22 20:03:59 +0200

    login design

--- src/pages/home/Login.jsx
+++ src/pages/home/Login.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import "./login.css"
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="pictureLeft">
+            <img src="/assets/login2.jpg" alt="" className="loginpic" />
+            </div>
+            <div className="form">
+            <label for="uname"><b>Username</b></label>
+            <input className='logininput' type="text" placeholder="Enter Username" name="uname"/>
+
+            <label for="psw"><b>Password</b></label>
+            <input className='logininput' type="password" placeholder="Enter Password" name="psw" />
+
+            <button className='buttonLogin' type="submit">Login</button>
+            <label>
+            </label>
+            </div>
+        </div>
+    </div>
   )
 }
